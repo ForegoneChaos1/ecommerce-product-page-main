e157a0f6c557b93cfacf3afe4d9ff072e404e892
Chaos Control <foregonechaos@gmail.com>
2026-09-04 10:30:46 +0100

    Responsiveness

--- index.js
+++ index.js
@@ -7,12 +7,36 @@ const thumbnails = document.querySelectorAll('.thumbnail-img img');
 const productDisplay = document.querySelector('.product');
 const productDisplayImgs = document.querySelectorAll('.product img');
 const currentDisplay = document.querySelectorAll('.highlight')
+const add = document.querySelector('.plus-icon')
+const minus = document.querySelector('.minus-icon')
+const quantity = document.querySelector('.quantity')
+const cartItems = document.querySelector('.cart-img')
+const cartOpen = document.querySelector('.cart-display')
+const displayPort = document.getElementsByName('section')
+let clickCount = 0;
+
+add.addEventListener('click', increaseProduct )
+minus.addEventListener('click', decreaseProduct )
+
+
+
+cartItems.addEventListener('click', () => {
+    clickCount++
+    cartOpen.style.display = 'block'
+    if (clickCount % 2 === 0 ) {
+        cartOpen.style.display = 'none'
+    }
+})
 
 const imgslide = [...thumbnails];
 const productImgslide = [...productDisplayImgs];
 const activeHighlight = [...currentDisplay];
+const cartDisplay = document.querySelector('.cart-img span')
 
-// console.log(activeHighlight)
+let counter = 0;
+
+const addToCartButton = document.querySelector('.shop-cart')
+addToCartButton.addEventListener('click', addToCart)
 
 
 for (let i = 0; i < imgslide.length; i++) {
@@ -34,6 +58,27 @@ for (let i = 0; i < imgslide.length; i++) {
             activeHighlight[i].classList.add('current')
         }
     })
+}
+function increaseProduct (){
+    counter = counter + 1
+    quantity.textContent = counter;
+}
+
+function decreaseProduct (){
+    counter = counter - 1
+    if (counter < 0) {
+        counter = 0;
+    }
+    quantity.textContent = counter;
+}
+
+function addToCart() {
+    if (counter <= 0) {
+        return;
+    }
+    cartDisplay.style.display = 'block';
+    cartDisplay.textContent = counter;
+
 }
 
 
